e0a737cdd28c2d2f29bf2398447e8e688a994298
jpmf12Z <joaopedromf12@edu.uniube.br>
2026-05-18 17:27:22 -0300

    Add files via upload

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer>
+  <p>Troca de livros © 2025</p>
+</footer>
+</body>
+</html>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8">
+  <title>Troca de livros - Teste</title>
+</head>
+<body>
+<header>
+  <h1>Troca de livros</h1>
+  <nav>
+    <a href="/SistemaIntercambioDeLivros/">Inicio</a>
+  </nav>
+</header>
+<main>
