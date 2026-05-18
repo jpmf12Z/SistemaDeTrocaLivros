7f2257c2cad2bc1db599608fc03eeb31b60f2c54
jpmf12Z <joaopedromf12@edu.uniube.br>
2026-05-18 17:27:06 -0300

    Add files via upload

---
+++ app/Views/home/index.php
@@ -0,0 +1,11 @@
+<?php include __DIR__ . '/../templates/header.php'; ?>
+<h2>Bem Vindo a Troca de livros</h2>
+<p>O Sistema esta Funcionando! 🚀</p>
+
+<h3>Navegação</h3>
+<ul>
+  <li><a href="/SistemaIntercambioDeLivros/index.php?route=book/list">📚 Gerenciar livros</a></li>
+  <li><a href="/SistemaIntercambioDeLivros/index.php?route=trade/list">🔄 Ver propostas comerciais</a></li>
+</ul>
+
+<?php include __DIR__ . '/../templates/footer.php'; ?>
